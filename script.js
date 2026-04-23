e483e0eb2f05af4898acd3cadf0e3c6fb598b744
ellymichend-svg <ellymichend@gmail.com>
2026-04-23 21:19:45 +0800

    Update script.js

--- script.js
+++ script.js
@@ -77,6 +77,9 @@ function renderPage(page) {
                                 <p class="text-sm font-bold">${item.price}</p>
                                 <a href="data.html?id=${item.id}" class="bg-black text-white p-3 rounded-2xl">
                                     </a>
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                                 </svg>
                             </div>
                         </div>
                     </div>
